8efe37504fe43600c516434c0cc0ccaf873817fd
Avery Brinkman <averybrinkman@gmail.com>
2024-02-03 15:49:27 -0500

    app tweaks

--- Assignment2/flaskapp/flaskapp.py
+++ Assignment2/flaskapp/flaskapp.py
@@ -1,9 +1,9 @@
+from backend import login, signup, userInfo, verifyUser
 from create_database import create_database
 from database import getUserInfo
 from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, session
 import os
-from backend import login, signup, userInfo, verifyUser
 
 # Constants
 ROOT = "/var/www/html/flaskapp"
@@ -32,10 +32,13 @@ def signup_page():
         return redirect("/me")
 
     if request.method == "POST":
-        if signup(
-            request.form["user_name"], request.form["pswrd"], request.form["c_pswrd"]
-        ):
-            return redirect("signup/info")
+        user = signup(
+            request.form["user_name"],
+            request.form["pswrd"],
+            request.form["c_pswrd"],
+        )
+        if user != None:
+            return redirect("/signup/info")
 
     return render_template("signup.html")
 
@@ -64,13 +67,14 @@ def signup_info_page():
 def login_page():
     if request.method == "POST":
         # Try to login
-        if login(request.form["user_name"], request.form["pswrd"]):
+        user = login(request.form["user_name"], request.form["pswrd"])
+        if user != None:
             # Check if info needs to be added
             if getUserInfo(session["id"]) == None:
-                return redirect("signup/info")
+                return redirect("/signup/info")
 
-            # Otherwise show user info
-            return redirect("me")
+            # Otherwise go to homepage
+            return redirect("/")
 
     return render_template("login.html")
 
@@ -83,18 +87,21 @@ def me_page():
         userInfo = getUserInfo(session["id"])
 
         # Check that we have info
-        if userInfo != None:
-            # Show page w user info
-            return render_template(
-                "me.html",
-                userName=userInfo.user.userName,
-                firstName=userInfo.firstName,
-                lastName=userInfo.lastName,
-                email=userInfo.email,
-            )
+        if userInfo == None:
+            # Redirect to enter info if missing
+            return redirect("/signup/info")
 
-    # Redirect to login if we failed at any step of the way
-    return redirect("login")
+        # Show page w user info
+        return render_template(
+            "me.html",
+            userName=userInfo.user.userName,
+            firstName=userInfo.firstName,
+            lastName=userInfo.lastName,
+            email=userInfo.email,
+        )
+
+    # Redirect to login
+    return redirect("/login")
 
 
 if __name__ == "__main__":
