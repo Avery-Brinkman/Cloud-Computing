47fbd9e583c00ad6e227d600ea6864aebb5ba1c5
Avery Brinkman <averybrinkman@gmail.com>
2024-02-03 15:48:34 -0500

    rework backend

--- Assignment2/flaskapp/backend.py
+++ Assignment2/flaskapp/backend.py
@@ -1,8 +1,8 @@
-import os
 import bcrypt
 from database import createUser, addUserInfo, getUser, getUserFromLogin
 from flask import session
 import jwt
+import os
 from User import User
 
 
@@ -11,28 +11,38 @@ def checkPassword(plaintext: str, hashed: str) -> bool:
     return bcrypt.checkpw(bytes(plaintext, "utf-8"), bytes(hashed, "utf-8"))
 
 
-# Checks session info to verify user
+# Checks session info to verify user. Removes info if invalid
 def verifyUser() -> bool:
     # Check for session values
-    if "id" in session and "jwt" in session:
+    if ("id" in session) and ("jwt" in session):
         # Get user object by id
         user = getUser(session["id"])
 
         # Check that we got a user
         if user == None:
+            removeFromSession()
             return False
 
         # Get jwt
         try:
-            payload = jwt.decode(session["jwt"], os.getenv("SECRET_KEY"))
+            payload = jwt.decode(
+                session["jwt"], os.getenv("SECRET_KEY"), algorithms=["HS256"]
+            )
         except jwt.exceptions.InvalidTokenError:
+            removeFromSession()
             return False
 
         # Check that payload has required values
-        if "id" in payload and "userName" in payload:
+        if ("id" in payload) and ("userName" in payload):
             # Check that values match the database
-            return payload["id"] == user.id and payload["userName"] == user.userName
+            valid = (payload["id"] == user.id) and (
+                payload["userName"] == user.userName
+            )
+            if not valid:
+                removeFromSession()
+            return valid
 
+    removeFromSession()
     return False
 
 
@@ -43,45 +53,45 @@ def addToSession(user: User):
     session["jwt"] = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256")
 
 
-# Checks user credentials, and adds info to session if valid
-def login(userName: str, password: str) -> bool:
+# Removes session values
+def removeFromSession():
+    session.pop("id", None)
+    session.pop("jwt", None)
+
+
+# Checks user credentials and returns user (or none on invalid creds)
+def login(userName: str, password: str) -> User | None:
     # Get user info
     user = getUserFromLogin(userName)
 
-    # Check that we got a user
-    if user == None:
-        return False
-
-    # Check password
-    if checkPassword(password, user.password):
-        # Set session data
+    # Check that we got a user with valid password
+    if (user != None) and checkPassword(password, user.password):
+        # Log user in if successful
         addToSession(user)
+        return user
 
-        return True
-
-    return False
+    return None
 
 
-def signup(userName: str, password: str, confirmPassword: str):
+# Creates new user, and returns created user
+def signup(userName: str, password: str, confirmPassword: str) -> User | None:
     if password != confirmPassword:
-        return False
+        return None
 
     # Hash password
     passHash = bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt())
     # Create user and add to database
     user = createUser(userName, passHash.decode("utf-8"))
 
-    if user == None:
-        return False
+    # Log user in if successful
+    if user != None:
+        addToSession(user)
 
-    # Add session info
-    addToSession(user)
-
-    return True
+    return user
 
 
 def userInfo(firstName: str, lastName: str, email: str):
     user = getUser(session["id"])
     if user == None:
-        return
+        raise Exception(f'Could not find user {session["id"]} to add info to')
     addUserInfo(user, firstName, lastName, email)
