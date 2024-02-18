e72eaebda87997d09d2d5f5c5c1e67aa1eec7d9b
Avery Brinkman <averybrinkman@gmail.com>
2024-02-18 13:51:44 -0500

    Refactor file paths and improve word parsing

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,13 +1,18 @@
 import os
 import socket
 
+INPUT = "/home/data"
+OUTPUT = "/home/output"
+
 
 # List all the text files in /home/data (A)
 def listTextFiles() -> str:
-    out = ""
-    files = os.listdir("/home/data")
-    out += "Text files in /home/data:\n"
+    out = "Text files in /home/data:\n"
+    # Get the files
+    files = os.listdir(INPUT)
+    # Add the files to the output
     for f in files:
+        # Only add .txt files
         if f.endswith(".txt"):
             out += f"  {f}\n"
     return out
@@ -15,23 +20,33 @@ def listTextFiles() -> str:
 
 # Open the file and parse the words (B)
 def parseFile(file: str) -> dict:
-    with open(file, "r") as f:
-        words = f.read().lower().split()
-        wordCounts = {word: words.count(word) for word in words}
-        wordCounts["__total__"] = len(words)
-        return wordCounts
-
-    return {"__total__": 0}
+    wordCounts = {"__total__": 0}
+    # Open file in utf-8
+    with open(file, "r", encoding="utf-8") as f:
+        # Read and lowercase the file
+        words = f.read().lower()
+        # Replace dashes with spaces and split
+        words = words.replace("—", " ").split()
+        for word in words:
+            # Strip punctuation
+            word = word.strip(".,!?;:‘’")
+            # Add to the dictionary
+            wordCounts[word] = wordCounts.get(word, 0) + 1
+            # Increment total
+            wordCounts["__total__"] = wordCounts.get("__total__", 0) + 1
+    # Return the dictionary
+    return wordCounts
 
 
 # Open IF.txt and Limerick-1.txt and get the word counts in each file (C & D)
 def getWordCounts() -> str:
     out = ""
-    ifFile = parseFile("/home/data/IF.txt")
-    limerick = parseFile("/home/data/Limerick-1.txt")
+    # Parse the files
+    ifFile = parseFile(f"{INPUT}/IF.txt")
+    limerick = parseFile(f"{INPUT}/Limerick-1.txt")
 
     # Display info
-    out += f"        IF.txt word count: {ifFile['__total__']}\n"
+    out += f"IF.txt word count        : {ifFile['__total__']}\n"
     out += f"Limerick-1.txt word count: {limerick['__total__']}\n"
     out += "\n"
     out += f"Total word count: {ifFile['__total__'] + limerick['__total__']}\n"
@@ -43,6 +58,7 @@ def getWordCounts() -> str:
     # Sort
     sortedIF = sorted(ifFile.items(), key=lambda x: x[1], reverse=True)
     out += "IF.txt maximum word counts:\n"
+    # Get the top 3
     for i in range(3):
         out += f"  {sortedIF[i][0]}: {sortedIF[i][1]}\n"
 
@@ -58,7 +74,7 @@ def main():
     output = listTextFiles() + "\n" + getWordCounts() + "\n" + getIP()
 
     # (F)
-    with open("/home/output/result.txt", "w+") as f:
+    with open(f"{OUTPUT}/result.txt", "w") as f:
         f.write(output)
 
     # (G)
